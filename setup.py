729854ff389408e1c4b904dc94af56cb39d3bc39
RandriaNtsoa28 <125382866+RandriaNtsoa28@users.noreply.github.com>
2024-01-29 14:18:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
-name='{name-of-your-project}',
+name='{quiz_game}',
 version='1.0',
-author='{your-full-name-or-your-github-username}',
+author='{RandriaNtsoa28}',
 license='MIT',
 long_description=open('README.md').read(),
 )
